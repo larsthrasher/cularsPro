5c3b6b54b7bb4693abcb050f2ecc7f795392d43f
larsthrasher <thrasherlars@gmail.com>
2020-05-07 09:32:11 -0400

    SingleColorPalette navbar and footer

--- src/Navbar.js
+++ src/Navbar.js
@@ -25,15 +25,16 @@ class Navbar extends Component {
     this.setState({ open: false });
   }
   render(){
-    const {level, changeLevel} = this.props;
+    const {level, changeLevel, showingAllColors } = this.props;
     const { format } = this.state;
     return (
       <header className= "Navbar">
         <div className='logo'>
           <Link to="/">cularsPro</Link>
         </div>
+      {showingAllColors && (
         <div className="slider-container">
-        <span>Level: {level}</span>
+          <span>Level: {level}</span>
           <div className="slider">
               <Slider
                 defaultValue={level}
@@ -44,6 +45,7 @@ class Navbar extends Component {
               />
           </div>
         </div>
+      )}
         <div className="select-container">
           <Select value={format} onChange={this.handleFormatChange}>
             <MenuItem value="hex">HEX - #fffff</MenuItem>
